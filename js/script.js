d1022b11ed39e91c443054dbc308874cd0229678
Principe Matias <principematias23@gmail.com>
2022-06-26 23:11:20 -0300

    pagina de musica

---
+++ js/script.js
@@ -0,0 +1,9 @@
+//Menu Hamburguesa
+const menuHamburguesa = document.getElementById('menu');
+menuHamburguesa.addEventListener('click', () => {
+    document.getElementById('navLinks').classList.toggle('nav__link--action');
+    document.getElementById('lineOne').classList.toggle('nav__img-linea--1');
+    document.getElementById('lineTwo').classList.toggle('nav__img-linea--2');
+    document.getElementById('lineThree').classList.toggle('nav__img-linea--3');
+    document.querySelector('body').classList.toggle('body');
+});
